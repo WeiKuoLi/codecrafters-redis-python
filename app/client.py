ec9d02dcb5f77cbf10d7d09a4ccb2e80244f9d93
WeiKuoLi <wli22@nd.edu>
2024-02-10 15:50:07 -0500

    3rd staget debug

--- app/client.py
+++ app/client.py
@@ -7,22 +7,29 @@ server_port = 6379      # Replace with the server's actual port number
 # Create a socket object for the client
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print("clien socket initiated")
+print("Client socket initiated")
 
-# Connect the client socket to the server
-client_socket.connect((server_ip, server_port))
-
-# Send data to the server
-message = "*1\r\n$4\r\nping\r\n"
 try:
+    # Connect the client socket to the server
+    client_socket.connect((server_ip, server_port))
+    print("Connected to server")
+
+    # Send data to the server
+    message = "*1\r\n$4\r\nping\r\n"
     client_socket.send(message.encode())
-except:
-    print("message sent failed")
+    print("Message sent")
 
-# Receive data from the server
-data = client_socket.recv(1024)
-print("Received data from server:", data.decode())
+    # Receive data from the server
+    data = client_socket.recv(1024)
+    print("Received data from server:", data.decode())
 
-# Close the connection
-client_socket.close()
+except ConnectionRefusedError:
+    print("Connection refused: Server is not running or unreachable")
+except Exception as e:
+    print("An error occurred:", e)
+
+finally:
+    # Close the connection
+    client_socket.close()
+    print("Client socket closed")
 
