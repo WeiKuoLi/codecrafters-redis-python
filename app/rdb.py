22327171e155b097662028265cca8540fdaab667
WeiKuoLi <wli22.nd.edu>
2024-02-13 23:57:49 -0500

    rdb-px-typ-err

--- app/rdb.py
+++ app/rdb.py
@@ -237,17 +237,13 @@ async def import_rdb_file(redis_server, file_path=None):
                 _expire_time -= current_timestamp
                 #print(f"EXPIRE in {_expire_time} ms")
                 if (_expire_time > 0):
-                    redis_server.command_set(_key, _value, "px", _expire_time)
-                    #redis_server.redis[str(_key)] = RedisObject(str(_value))
-                    #asyncio.create_task(redis_handler.delete_key(str(_key), _expire_time))
+                    redis_server.command_set(_key, _value, RedisObject("px"), _expire_time)
             elif(block_typ == 'fd'):
                 _expire_time, _val_typ, _key, _value = block_data
                 _expire_time -= current_timestamp/1000
                 assert _val_typ == 0
                 if (_expire_time > 0):
-                    redis_server.command_set(_key, _value, "px", _expire_time * 1000)
-                    #redis_handler.redis[str(_key)] = RedisObject(str(_value))
-                    #asyncio.create_task(redis_handler.delete_key(str(_key), _expire_time * 1000))
+                    redis_server.command_set(_key, _value, RedisObject("px"), _expire_time * 1000)
                 
             #print(f"{block_typ} block,{block_data}\n\n")
             #block_typ, expire_time, val_typ, key, value =
