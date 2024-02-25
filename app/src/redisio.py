ac1d0b5ef026cbe519f43a8c59da372aa1bb4499
WeiKuoLi <wli22@nd.edu>
2024-02-25 16:52:22 -0500

    test

--- app/src/redisio.py
+++ app/src/redisio.py
@@ -56,6 +56,7 @@ class RedisIOHandler:
             if(len(input_redisobject.obj) == 1): 
                 _cmd = str(input_redisobject.obj[0].obj[0].obj)
                 output_redisobject = handler[_cmd](client_id=client_id, command=_cmd, *(input_redisobject.obj[0].obj[1:]))
+                print(output_redisobject.__repr__())
                 return output_redisobject
 
             output_redisobject = RedisObject(obj=[], typ='list')
@@ -63,6 +64,7 @@ class RedisIOHandler:
                 _cmd = str(_obj.obj[0].obj)
                 output_redisobject.obj.append( handler[_cmd](client_id=client_id, command=_cmd, *(_obj.obj[1:])))
 
+            print(output_redisobject.__repr__())
             return output_redisobject
 
         except:
