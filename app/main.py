46b4a6f9a04c3a99375ed64776946cff28b2d36e
WeiKuoLi <wli22@nd.edu>
2024-02-25 18:25:15 -0500

    test

--- app/main.py
+++ app/main.py
@@ -47,7 +47,7 @@ async def handle_master(reader, writer, redis_handler):
             #for debug
             #print(f"Response master: {redis_handler.parsed_output.__repr__()}")
             if(output_redisobject.typ == 'list' and output_redisobject.obj!=[]):
-                response_message = redis_handler.parse_output(output_redisobject)
+                #response_message = redis_handler.parse_output(output_redisobject)
                 print(f"Response master {response_message}")
                 
             '''
